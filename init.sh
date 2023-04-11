ec642afa16d9b7a41d6cf16cc7ebcdb5e1ac48a5
Namonay <105780726+Namonay@users.noreply.github.com>
2023-04-11 18:23:04 +0200

    Add files via upload

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@ git clone git@github.com:Namonay/42pouic.git youpi
 mkdir -p '/nfs/homes/'$USER'/.local/share/backgrnd'
 mv ''$PWD'/IMG_0837.jpg' '/nfs/homes/'$USER'/.local/share/backgrnd'
 gsettings set org.gnome.desktop.background picture-uri '/nfs/homes/'$USER'/.local/share/backgrnd/IMG_0837.jpg'
-rm backgrnd.sh
+rm ../youpi
 clear
