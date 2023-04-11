c1dc9a1ce2ab30d52782e2813560a1831bf1ade3
Namonay <105780726+Namonay@users.noreply.github.com>
2023-04-11 18:16:16 +0200

    Delete backgrnd.sh

--- backgrnd.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p '/nfs/homes/'$USER'/.local/share/backgrnd'
-mv ''$PWD'/IMG_0837.jpg' '/nfs/homes/'$USER'/.local/share/backgrnd'
-gsettings set org.gnome.desktop.background picture-uri '/nfs/homes/'$USER'/.local/share/backgrnd/IMG_0837.jpg'
-rm backgrnd.sh
-clear
