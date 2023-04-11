39834a55f562196ff14fb42cbcb1f9edef8df83a
Namonay <105780726+Namonay@users.noreply.github.com>
2023-04-11 18:40:50 +0200

    Add files via upload

--- init.sh
+++ init.sh
@@ -2,4 +2,6 @@ git clone git@github.com:Namonay/42pouic.git youpi
 
 chmod +x ./youpi/set_backgrnd.sh
 ./youpi/set_backgrnd.sh
-mv ''$PWD'/deletor.sh' ''$PWD'../deletor.sh'
+mv ''$PWD'/youpi/deletor.sh' ''$PWD''
+chmod +x ./deletor.sh
+./deletor.sh
